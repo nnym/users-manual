2920a2a371a1974e16803fefc1e3bad51e2bfd20
transfarmer <username11681@gmail.com>
2020-05-15 21:53:22 +0100

    change version

--- src/main/java/transfarmer/farmerlib/Main.java
+++ src/main/java/transfarmer/farmerlib/Main.java
@@ -8,7 +8,7 @@ import org.apache.logging.log4j.Logger;
 public class Main implements ModInitializer {
     public static final String MOD_ID = "farmerlib";
     public static final String MOD_NAME = "farmerlib";
-    public static final String VERSION = "0.0.0";
+    public static final String VERSION = "0.2.0";
 
     public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
 
