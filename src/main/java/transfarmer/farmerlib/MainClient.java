53ee90ebfda619238fe8e5ce660db88d2c743c3d
transfarmer <username11681@gmail.com>
2020-05-15 17:09:24 +0100

    remove debug event registration

--- src/main/java/transfarmer/farmerlib/MainClient.java
+++ src/main/java/transfarmer/farmerlib/MainClient.java
@@ -4,9 +4,6 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.MinecraftClient;
-import transfarmer.farmerlib.event.TranslationEvent;
-
-import static net.minecraft.util.ActionResult.FAIL;
 
 @Environment(EnvType.CLIENT)
 public class MainClient implements ClientModInitializer {
@@ -15,6 +12,5 @@ public class MainClient implements ClientModInitializer {
     @Override
     @Environment(EnvType.CLIENT)
     public void onInitializeClient() {
-        TranslationEvent.register(event -> event.setResult(FAIL));
     }
 }
