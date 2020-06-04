9a9caaf04ec5f958f6f8d152f432af0791692b48
transfarmer <username11681@gmail.com>
2020-06-04 19:05:40 +0100

    change ModRegistry Map to ArrayMap

--- src/main/java/user11681/usersmanual/registry/ModRegistry.java
+++ src/main/java/user11681/usersmanual/registry/ModRegistry.java
@@ -1,16 +1,18 @@
 package user11681.usersmanual.registry;
 
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 import javax.annotation.Nonnull;
 import net.minecraft.util.Identifier;
+import user11681.usersmanual.collections.ArrayMap;
+import user11681.usersmanual.collections.ArraySet;
+import user11681.usersmanual.collections.OrderedArrayMap;
 
 public class ModRegistry<T extends RegistryEntry> implements Iterable<T> {
-    protected final Map<Identifier, T> entries;
+    protected final ArrayMap<Identifier, T> entries;
 
     public ModRegistry() {
-        this.entries = new HashMap<>();
+        this.entries = new OrderedArrayMap<>();
     }
 
     public <U extends T> U register(final U entry) {
@@ -25,6 +27,18 @@ public class ModRegistry<T extends RegistryEntry> implements Iterable<T> {
 
     public T get(final Identifier identifier) {
         return this.entries.get(identifier);
+    }
+
+    public ArraySet<Identifier> keys() {
+        return this.entries.keySet();
+    }
+
+    public ArraySet<Map.Entry<Identifier, T>> entries() {
+        return this.entries.entrySet();
+    }
+
+    public ArraySet<T> values() {
+        return this.entries.values();
     }
 
     @Override
