35b4fb582884fe6ec83c0e33830d921ab7d8de84
transfarmer <username11681@gmail.com>
2020-06-04 18:45:56 +0100

    add AbstractRegistryEntry

---
+++ src/main/java/user11681/usersmanual/registry/AbstractRegistryEntry.java
@@ -0,0 +1,17 @@
+package user11681.usersmanual.registry;
+
+import net.minecraft.util.Identifier;
+
+public abstract class AbstractRegistryEntry implements RegistryEntry {
+    protected final Identifier identifier;
+
+    public AbstractRegistryEntry(final Identifier identifier) {
+
+        this.identifier = identifier;
+    }
+
+    @Override
+    public Identifier getIdentifier() {
+        return this.identifier;
+    }
+}
