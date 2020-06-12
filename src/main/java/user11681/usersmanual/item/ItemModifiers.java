c8d7257fa2db0c2abce468fd8c795596106ff65e
transfarmer <username11681@gmail.com>
2020-06-12 16:16:27 +0100

    add methods for making attack damage and speed attributes

--- src/main/java/user11681/usersmanual/item/ItemModifiers.java
+++ src/main/java/user11681/usersmanual/item/ItemModifiers.java
@@ -1,15 +1,35 @@
 package user11681.usersmanual.item;
 
+import net.minecraft.entity.attribute.EntityAttributeModifier;
 import net.minecraft.item.Item;
 
-import java.util.UUID;
-
 public final class ItemModifiers extends Item {
-    public static final UUID ATTACK_DAMAGE_MODIFIER_ID = Item.ATTACK_DAMAGE_MODIFIER_ID;
-    public static final UUID ATTACK_SPEED_MODIFIER_ID = Item.ATTACK_SPEED_MODIFIER_ID;
-
     private ItemModifiers() {
         //noinspection ConstantConditions
         super(null);
+    }
+
+    public static EntityAttributeModifier createAttackSpeedModifier(final int value) {
+        return createAttackSpeedModifier(value, EntityAttributeModifier.Operation.ADDITION);
+    }
+
+    public static EntityAttributeModifier createAttackSpeedModifier(final int value, final EntityAttributeModifier.Operation operation) {
+        return createAttackSpeedModifier("Weapon modifier", value, operation);
+    }
+
+    public static EntityAttributeModifier createAttackSpeedModifier(final String name, final int value, final EntityAttributeModifier.Operation operation) {
+        return new EntityAttributeModifier(ATTACK_SPEED_MODIFIER_ID, name, value, operation);
+    }
+
+    public static EntityAttributeModifier createAttackDamageModifier(final int value) {
+        return createAttackDamageModifier(value, EntityAttributeModifier.Operation.ADDITION);
+    }
+
+    public static EntityAttributeModifier createAttackDamageModifier(final int value, final EntityAttributeModifier.Operation operation) {
+        return createAttackDamageModifier("Weapon modifier", value, operation);
+    }
+
+    public static EntityAttributeModifier createAttackDamageModifier(final String name, final int value, final EntityAttributeModifier.Operation operation) {
+        return new EntityAttributeModifier(ATTACK_DAMAGE_MODIFIER_ID, name, value, operation);
     }
 }
