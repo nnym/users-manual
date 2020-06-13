ae5c4c1c41fab20eb56624339101f604c1522b04
transfarmer <username11681@gmail.com>
2020-06-14 00:14:54 +0100

    fix equal `EntityAttributeModifier#uuid`s being different objects

--- src/main/java/user11681/usersmanual/item/ItemModifiers.java
+++ src/main/java/user11681/usersmanual/item/ItemModifiers.java
@@ -1,14 +1,17 @@
 package user11681.usersmanual.item;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
-import net.minecraft.item.Item;
 
-public final class ItemModifiers extends Item {
-    public static final UUID ATTACK_DAMAGE_MODIFIER_ID = Item.ATTACK_DAMAGE_MODIFIER_ID;
-    public static final UUID ATTACK_SPEED_MODIFIER_ID = Item.ATTACK_SPEED_MODIFIER_ID;
+public final class ItemModifiers {
+    public static final List<UUID> RESERVED_IDENTIFIERS = new ArrayList<>();
 
-    private ItemModifiers() {
-        //noinspection ConstantConditions
-        super(null);
+    public static final UUID ATTACK_DAMAGE_MODIFIER_ID = UUID.fromString("CB3F55D3-645C-4F38-A497-9C13A33DB5CF");
+    public static final UUID ATTACK_SPEED_MODIFIER_ID = UUID.fromString("FA233E1C-4180-4865-B01B-BCCE9785ACA3");
+
+    static {
+        RESERVED_IDENTIFIERS.add(ItemModifiers.ATTACK_DAMAGE_MODIFIER_ID);
+        RESERVED_IDENTIFIERS.add(ItemModifiers.ATTACK_SPEED_MODIFIER_ID);
     }
 }
---
+++ src/main/java/user11681/usersmanual/mixin/mixin/entity/EntityAttributeModifierMixin.java
@@ -0,0 +1,31 @@
+package user11681.usersmanual.mixin.mixin.entity;
+
+import java.util.UUID;
+import net.minecraft.entity.attribute.EntityAttributeModifier;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import user11681.usersmanual.item.ItemModifiers;
+
+@Mixin(EntityAttributeModifier.class)
+public abstract class EntityAttributeModifierMixin {
+    @Shadow
+    @Final
+    @Mutable
+    private UUID uuid;
+
+    @Inject(method = "<init>(Ljava/util/UUID;Ljava/lang/String;DLnet/minecraft/entity/attribute/EntityAttributeModifier$Operation;)V", at = @At("RETURN"))
+    public void construct(final UUID uuid, final String name, final double value, final EntityAttributeModifier.Operation operation, final CallbackInfo info) {
+        for (final UUID identifier : ItemModifiers.RESERVED_IDENTIFIERS) {
+            if (identifier.equals(uuid)) {
+                this.uuid = identifier;
+
+                return;
+            }
+        }
+    }
+}
