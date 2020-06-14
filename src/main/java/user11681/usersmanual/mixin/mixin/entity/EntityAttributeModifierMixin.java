5ac78246f3860546bd4620d2d7fd6142d64c7766
transfarmer <username11681@gmail.com>
2020-06-14 01:07:45 +0100

    remove unnecessary looping

--- src/main/java/user11681/usersmanual/mixin/mixin/entity/EntityAttributeModifierMixin.java
+++ src/main/java/user11681/usersmanual/mixin/mixin/entity/EntityAttributeModifierMixin.java
@@ -20,12 +20,8 @@ public abstract class EntityAttributeModifierMixin {
 
     @Inject(method = "<init>(Ljava/util/UUID;Ljava/lang/String;DLnet/minecraft/entity/attribute/EntityAttributeModifier$Operation;)V", at = @At("RETURN"))
     public void construct(final UUID uuid, final String name, final double value, final EntityAttributeModifier.Operation operation, final CallbackInfo info) {
-        for (final UUID identifier : ItemModifiers.RESERVED_IDENTIFIERS) {
-            if (identifier.equals(uuid)) {
-                this.uuid = identifier;
-
-                return;
-            }
+        if (ItemModifiers.RESERVED_IDENTIFIERS.contains(uuid)) {
+            this.uuid = uuid;
         }
     }
 }
