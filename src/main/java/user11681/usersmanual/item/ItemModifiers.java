fd11298de68b020ee412c0f15735b48f343666e6
transfarmer <username11681@gmail.com>
2020-06-14 01:07:08 +0100

    set ItemModifiers field values to Item field values

--- src/main/java/user11681/usersmanual/item/ItemModifiers.java
+++ src/main/java/user11681/usersmanual/item/ItemModifiers.java
@@ -3,15 +3,20 @@ package user11681.usersmanual.item;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import net.minecraft.item.Item;
 
-public final class ItemModifiers {
+public final class ItemModifiers extends Item {
     public static final List<UUID> RESERVED_IDENTIFIERS = new ArrayList<>();
 
-    public static final UUID ATTACK_DAMAGE_MODIFIER_ID = UUID.fromString("CB3F55D3-645C-4F38-A497-9C13A33DB5CF");
-    public static final UUID ATTACK_SPEED_MODIFIER_ID = UUID.fromString("FA233E1C-4180-4865-B01B-BCCE9785ACA3");
+    public static final UUID ATTACK_DAMAGE_MODIFIER_ID = Item.ATTACK_DAMAGE_MODIFIER_ID;
+    public static final UUID ATTACK_SPEED_MODIFIER_ID = Item.ATTACK_SPEED_MODIFIER_ID;
 
     static {
         RESERVED_IDENTIFIERS.add(ItemModifiers.ATTACK_DAMAGE_MODIFIER_ID);
         RESERVED_IDENTIFIERS.add(ItemModifiers.ATTACK_SPEED_MODIFIER_ID);
+    }
+
+    private ItemModifiers(final Settings settings) {
+        super(settings);
     }
 }
