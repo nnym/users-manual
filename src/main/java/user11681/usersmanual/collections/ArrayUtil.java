7e9ec736a603c3c90c7178089c5159afaa192152
transfarmer <username11681@gmail.com>
2020-06-15 16:25:10 +0100

    merge `ArrayUtil`s

--- src/main/java/user11681/usersmanual/collections/ArrayUtil.java
+++ src/main/java/user11681/usersmanual/collections/ArrayUtil.java
@@ -1,13 +1,25 @@
 package user11681.usersmanual.collections;
 
+import java.util.Arrays;
+
+@SuppressWarnings("unchecked")
 public class ArrayUtil {
-    @SuppressWarnings("unchecked")
     public static <T> T[] comparable(final int length) {
         return (T[]) new Comparable[length];
     }
 
-    @SuppressWarnings("unchecked")
     public static <T> T[] create(final int length) {
         return (T[]) new Object[length];
+    }
+
+    @SafeVarargs
+    public static <T, U extends T> T[] append(final T[] array, final U... items) {
+        final int length = array.length;
+        final int appendixLength = items.length;
+        final T[] newArray = Arrays.copyOf(array, length + appendixLength);
+
+        System.arraycopy(items, 0, array, length, appendixLength);
+
+        return newArray;
     }
 }
